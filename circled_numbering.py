0cf5f6414ad979edaaedd282c374d0eeb96e3e2e
Aochen <aochen@pku.edu.cn>
2022-06-11 21:08:35 +0800

    Update circled_numbering.py

--- circled_numbering.py
+++ circled_numbering.py
@@ -13,18 +13,18 @@
 
 
 #使用此程序前，需要将py程序和文章放在同一个路径下，并将文章命名为"passage.txt"
-#标好的文件会被命名为'passageNumbered.txt'
+#标好的文件会被命名为'passage_numbered.txt'
 
 #This program requires the passage file under the same directory with the py file
 #The passage file should be named "passage.txt".
-#The numbered version is named 'passageNumbered.txt'
+#The numbered version is named 'passage_numbered.txt'
 
 
 
 circledNumArray = ["①","②","③","④","⑤","⑥","⑦","⑧","⑨","⑩","⑪","⑫","⑬","⑭"
 "⑮","⑯","⑰","⑱","⑲","⑳","㉑","㉒","㉓","㉔","㉕","㉖","㉗","㉘","㉙","㉚"]
 i = 0
-outputFile = open('passageNumbered.txt','w')
+outputFile = open('passage_numbered.txt','w')
 
 with open('passage.txt','r') as inputFile:
     line = inputFile.readline()
